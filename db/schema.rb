2087af577bf0b244bccc17751951f24bf120a8ec
Alexey Kanarskiy <kanarskiy.oa@gmail.com>
2018-04-19 16:59:25 +0300

    Generate book scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180419120416) do
+ActiveRecord::Schema.define(version: 20180419135616) do
+
+  create_table "books", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
 
   create_table "dummy_models", force: :cascade do |t|
     t.string "name"
