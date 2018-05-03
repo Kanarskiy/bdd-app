8eece500e2823b20e3bda4217570557b2b0b0366
Alexey Kanarskiy <kanarskiy.oa@gmail.com>
2018-05-03 17:13:49 +0300

    Updating and deleting books from the inventory

--- features/step_definitions/book_inventory_steps.rb
+++ features/step_definitions/book_inventory_steps.rb
@@ -7,6 +7,7 @@ Then('I should see the list of my books') do
     expect(page).to have_content('Moby Dick')
 end
 
+
 When('I submit a new book to my inventory') do
     click_link 'New Book'
 
@@ -20,4 +21,34 @@ Then('I should see the new book in my inventory') do
 
     expect(page).to have_content('Peace and War')
     expect(page).to have_content('Lev Tolstoy')
+end
+
+
+Given('I have a book in my inventory') do
+    FactoryBot.create(:book, :user => @registered_user, :name => 'War and Peace', :author => 'Lev Tolstoy')
+end
+When('I change the title of my book') do
+    visit root_path
+
+    click_link 'Edit'
+
+    fill_in 'book_name', :with => 'Art of war'
+
+    click_button 'Update Book'
+end
+Then('I should see the book with the new title in my inventory') do
+    visit root_path
+
+    expect(page).to_not have_content('War and Peace')
+    expect(page).to have_content('Art of war')
+end
+
+When('I remove a book from my inventory') do
+    visit root_path
+
+    click_link 'Destroy'
+end
+
+Then('I should not see it listing in the inventory anymore') do
+    expect(page).to_not have_content('War and Peace')
 end
--- spec/controllers/book_controller_spec.rb
+++ spec/controllers/book_controller_spec.rb
@@ -31,31 +31,92 @@ Rspec.describe BooksController do
             post :create, :params => {:book => params}
             expect(book).to have_received(:save)
         end
-    end
 
-    context 'when the book is succesfully saved' do
-        before do
-            allow(book).to receive(:save).and_return(true)
-            post :create, :params => {:book => params}
-        end
-
-        it 'redirects to the book show page' do
-            expect(response).to redirect_to(book_path(book))
-        end
-
-        it 'redirects to the book show page' do
-            expect(flash[:notice]).to eq('Book was successfully created.')
-        end
-    end
-
-    context 'when the book can’t be saved' do
-        before do
-            allow(book).to receive(:save).and_return(false)
-            post :create, :params => {:book => params}
-
-            it 'redirects back to the new page' do
-                expect(response).to render_template(:new)
+        context 'when the book is succesfully saved' do
+            before do
+                allow(book).to receive(:save).and_return(true)
+                post :create, :params => {:book => params}
             end
+
+            it 'redirects to the book show page' do
+                expect(response).to redirect_to(book_path(book))
+            end
+
+            it 'redirects to the book show page' do
+                expect(flash[:notice]).to eq('Book was successfully created.')
+            end
+        end
+
+        context 'when the book can’t be saved' do
+            before do
+                allow(book).to receive(:save).and_return(false)
+                post :create, :params => {:book => params}
+
+                it 'redirects back to the new page' do
+                    expect(response).to render_template(:new)
+                end
+            end
+        end
+    end
+
+    describe 'PATCH #update' do
+        let(:book) { FactoryBot.build_stubbed(:book) }
+
+        before do
+            allow(Book).to receive(:find).and_return(book)
+            allow(book).to receive(:update).and_return(true)
+        end
+
+        it 'updates the book' do
+            patch :update, :params => {
+                                 :id => book.id,
+                                 :book => {:name => 'New Name'}
+                         }
+        end
+
+        context 'when the update succeeds' do
+            it 'redirects to the book page' do
+                patch :update, :params => {
+                                     :id => book.id,
+                                     :book => {:name => 'New Name'}
+                             }
+
+                expect(response).to redirect_to(book_path(book))
+            end
+        end
+
+        context 'when the update failed' do
+            before do
+                allow(book).to receive(:update).and_return(false)
+            end
+
+            it 'renders the edit page again' do
+                patch :update, :params => {
+                                     :id => book.id,
+                                     :book => {:name => 'New Name'}
+                             }
+
+                expect(response).to render_template(:edit)
+            end
+        end
+    end
+
+    describe 'DELETE #destroy' do
+        let(:book) { FactoryBot.build_stubbed(:book) }
+
+        before do
+            allow(Book).to receive(:find).and_return(book)
+            allow(book).to receive(:destroy)
+
+            delete :destroy, :params => {:id => book.id}
+        end
+
+        it 'deletes the book' do
+            expect(book).to have_received(:destroy)
+        end
+
+        it 'redirects to the index page' do
+            expect(response).to redirect_to(books_path)
         end
     end
 end
