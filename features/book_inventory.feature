a91788cf91b831060bee40f8ceff4c664c26c4bc
Alexey Kanarskiy <kanarskiy.oa@gmail.com>
2018-05-03 16:09:29 +0300

    Add books to the inventory

--- features/book_inventory.feature
+++ features/book_inventory.feature
@@ -5,9 +5,14 @@ Feature: Book Inventory
 
   Background:
     Given I am a registered user
-    And I am logged in
+    And   I am logged in
 
 Scenario: Listing books in my inventory
   Given I have populated my inventory with several books
-  When I visit the homepage
-  Then I should see the list of my books
+  When  I visit the homepage
+  Then  I should see the list of my books
+
+Scenario: Adding a new book to the inventory
+  When I submit a new book to my inventory
+  Then I should see the new book in my inventory
+
--- features/step_definitions/book_inventory_steps.rb
+++ features/step_definitions/book_inventory_steps.rb
@@ -6,3 +6,18 @@ Then('I should see the list of my books') do
     expect(page).to have_content('Don Quixote')
     expect(page).to have_content('Moby Dick')
 end
+
+When('I submit a new book to my inventory') do
+    click_link 'New Book'
+
+    fill_in 'book_name', :with => 'Peace and War'
+    fill_in 'book_author', :with => 'Lev Tolstoy'
+
+    click_button 'Create Book'
+end
+Then('I should see the new book in my inventory') do
+    visit root_path
+
+    expect(page).to have_content('Peace and War')
+    expect(page).to have_content('Lev Tolstoy')
+end
